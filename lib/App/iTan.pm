264aa20a58d439f748dfec35932ea1c8682d225e
Maros Kollar <maros@k-1.com>
2012-06-02 17:38:36 +0200

    Release 1.04

--- lib/App/iTan.pm
+++ lib/App/iTan.pm
@@ -9,7 +9,7 @@ use MooseX::App qw(Color);
 
 app_namespace 'App::iTan::Command';
 
-our $VERSION = '1.03';
+our $VERSION = '1.04';
 our $AUTHORITY = 'cpan:MAROS';
 
 __PACKAGE__->meta->make_immutable;
