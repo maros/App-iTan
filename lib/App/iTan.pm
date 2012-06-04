43cde703e502f6bb6b020f1c5d00f801649192c1
Maros Kollar <maros@k-1.com>
2012-06-04 11:47:12 +0200

    Fix pod error

--- lib/App/iTan.pm
+++ lib/App/iTan.pm
@@ -64,7 +64,7 @@ L<App::iTan::Command::Help>
 Imports a list of iTans into the database
 L<App::iTan::Command::Import>
 
-=item * info    
+=item * info
 
 Info about the selected iTAN
 L<App::iTan::Command::info>
@@ -107,6 +107,6 @@ App::iTan is Copyright (c) 2012 Maroš Kollár
 This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-=end
+=cut
 
 'Sponsored by Lehman Brothers Holdings Inc.';
